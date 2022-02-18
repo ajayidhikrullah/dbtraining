ca52d602faf697558dcd21415fc116d208a2c859
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-02-18 11:26:59 +0000

    added more contents + placed the headings correctly

--- successfactor.php
+++ successfactor.php
@@ -11,7 +11,7 @@
           <h1>SuccessFactor Module</h1>
           <h2>We are an expert in providing and rendering Successfactor, Information security and CyberSecurity training to individuals, professionals and corporate bodies.</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
-            <a href="#about" class="btn-get-started scrollto">...More About Us</a>
+            <a href="#about" class="btn-get-started scrollto">Read more on SAP Modules...</a>
             <!-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> -->
           </div>
         </div>
@@ -28,25 +28,22 @@
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-          <h2>Team</h2>
+          <h2>Training Modules</h2>
           <p><!-- etc --></p>
         </div>
 
         <div class="row">
-
           <div class="col-lg-6">
             <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
               <div class="pic"><img src="https://res.cloudinary.com/www-nairaproducts-ng/image/upload/v1610463126/img_s6iaru.png" class="img-fluid" alt=""></div>
               <div class="member-info">
-                <h4>Olu L</h4>
-                <span>Chief Executive Officer</span>
-                <!-- <p></p> -->
-                <div class="social">
-                  <a href=""><i class="ri-twitter-fill"></i></a>
-                  <a href=""><i class="ri-facebook-fill"></i></a>
-                  <a href=""><i class="ri-instagram-fill"></i></a>
-                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                </div>
+                <h4>Employee Central</h4>
+                <span>Discussion</span>
+                    <p><i>SuccessFactors Employee Central provides an option to manage key HR operations and contains HR master data like employee name, identification, user name, etc.</i><p>
+                    <p><em>* Employee Central Integration</em></p>
+                    <p><em>* Employee Central Payroll</em></p>
+                    <hr>
+                    <p>The following is a list of the supported countries in Employee Central Payroll: Argentina, Australia, Austria, Brazil, Canada, Chile, China, Colombia, Finland, France, Germany, Hong Kong, India, Ireland, Italy, Japan, Malaysia, Mexico, Netherlands, New Zealand, Qatar, Russia, Saudi Arabia, Singapore, South Africa, South Korea, Spain, Sweden, Switzerland, Taiwan, Thailand, United Arab Emirates, United Kingdom, United States, and Venezuela.</p>
               </div>
             </div>
           </div>
@@ -55,15 +52,17 @@
             <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
               <div class="pic"><img src="https://res.cloudinary.com/www-nairaproducts-ng/image/upload/v1610463126/img_s6iaru.png" class="img-fluid" alt=""></div>
               <div class="member-info">
-                <h4>NOJ</h4>
-                <span>Chief Technology Officer</span>
-                <!-- <p>Aut maiores voluptates amet et quis praesentium qui senda para</p> -->
-                <div class="social">
+                <h4>SAP SuccessFactor Performance and Goals</h4>
+                <span>Discussion</span>
+                <p>SAP Performance and Goal management helps people managers to align individual goals with the company business objectives and core values. It ensures that all the employees are working on same similar goals to achieve and eliminate gap and stay on the right path to achieve organization goals.
+                If a customer purchases Goal and Performance management together, then all the goals in Goal management are automatically populated in Performance management. Performance management helps managers to measure metrics as per employee performance to meet objectives as per company targets and objectives.</p>
+                
+                <!-- <div class="social">
                   <a href=""><i class="ri-twitter-fill"></i></a>
                   <a href=""><i class="ri-facebook-fill"></i></a>
                   <a href=""><i class="ri-instagram-fill"></i></a>
                   <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                </div>
+                </div> -->
               </div>
             </div>
           </div>
@@ -72,20 +71,26 @@
             <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="300">
               <div class="pic"><img src="https://res.cloudinary.com/www-nairaproducts-ng/image/upload/v1610463126/img_s6iaru.png" class="img-fluid" alt=""></div>
               <div class="member-info">
-                <h4>Tolu</h4>
-                <span>Manager</span>
-                <!-- <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p> -->
-                <div class="social">
+                <h4>SAP SuccessFactor Onboarding</h4>
+                <span>Discussion</span>
+                <p>Using SuccessFactors onboarding, you can engage new hires from the start. It is important to provide enhanced onboarding experience to the new hires to engage top talents from university.
+                    SuccessFactors Onboarding provides an exceptional, enhanced set of onboarding activities in an organization to manage new hires from the start.
+                    <p>Key Features of SuccessFactors Onboarding</p>
+                    <p>The key features of SuccessFactors Onboarding are as follows
+                    Hiring coordinator can initiate onboarding for hires recruited through SuccessFactors recruitment.
+                    Hiring process involves kickoff onboarding process in SuccessFactors for all hires and complete new hire steps.
+                    Onboarding involves follow up with hire to make sure that he has completed new hire steps and has taken advantage of the new hire portal.
+                    Onboarding can be integrated with recruitment and Employee central to manage new hire activities.</p>
+
+                <!-- <div class="social">
                   <a href=""><i class="ri-twitter-fill"></i></a>
                   <a href=""><i class="ri-facebook-fill"></i></a>
                   <a href=""><i class="ri-instagram-fill"></i></a>
                   <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                </div>
+                </div> -->
               </div>
             </div>
           </div>
-
-
         </div>
 
       </div>
